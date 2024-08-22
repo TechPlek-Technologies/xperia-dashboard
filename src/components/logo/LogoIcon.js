2fb3d0d4c5dc2fc610dfa5231bfb042cf96fbc8e
Deepak's PC <d33paksharma.39@gmail.com>
2024-08-22 13:10:48 +0530

    deployment setting

--- src/components/logo/LogoIcon.js
+++ src/components/logo/LogoIcon.js
@@ -1,14 +1,14 @@
 // material-ui
 import { useTheme } from '@mui/material/styles';
 
-import logoIcon from 'assets/images/newlogo.webp';
-import logoIconDark from 'assets/images/logo.webp';
+import logoIcon from 'assets/images/logo1.webp';
+import logoIconDark from 'assets/images/logo1.webp';
 import { ThemeMode } from 'config';
 
 const LogoIcon = () => {
   const theme = useTheme();
 
-  return <img src={theme.palette.mode === ThemeMode.DARK ? logoIconDark : logoIcon} alt="icon logo" width="200" />;
+  return <img src={theme.palette.mode === ThemeMode.DARK ? logoIconDark : logoIcon} alt="icon logo" width="28" height="28" />;
 };
 
 export default LogoIcon;
